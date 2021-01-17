1ebeb7a6592ed0360080f42013b568b86f222b29
Jmiguel14 <miguel.jurado@epn.edu.ec>
2021-01-17 10:11:09 -0500

    Integración de botón paypal checkut :(

--- pagar.php
+++ pagar.php
@@ -35,7 +35,57 @@ if ($_POST){
     }
 }
 ?>
+<script src="https://www.paypal.com/sdk/js?client-id=sb&currency=USD" data-sdk-integration-source="button-factory"></script>
 
+<div class="jumbotron text-center">
+    <h1 class="display-4">¡Paso Final!</h1>
+    <hr class="my-4">
+    <p class="lead">Estas a punto de pagar con PayPal la cantidad de:
+        <h4>$<?php echo number_format($total, 2); ?></h4>
+        <div id="smart-button-container">
+            <div style="text-align: center;">
+                <div id="paypal-button-container"></div>
+            </div>
+        </div>
+    </p>
+    <p>Los productos podrán ser descargados una vez que se procese el pago<br/>
+        <strong>(Para aclaraciones :app_distribuidas@gmail.com)</strong>
+    </p>
+</div>
+
+<script src="https://www.paypal.com/sdk/js?client-id=sb"></script>
+<script>paypal.Buttons().render('body');</script>
+
+<script>
+function initPayPalButton() {
+    paypal.Buttons({
+    style: {
+        shape: 'rect',
+        color: 'gold',
+        layout: 'vertical',
+        label: 'paypal',
+        
+    },
+
+    createOrder: function(data, actions) {
+        return actions.order.create({
+        purchase_units: [{"amount":{"currency_code":"USD","value":1}}]
+        });
+    },
+
+    onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+        alert('Transaction completed by ' + details.payer.name.given_name + '!');
+        });
+    },
+
+    onError: function(err) {
+        console.log(err);
+    }
+    }).render('#paypal-button-container');
+}
+initPayPalButton();
+</script>
 
 <?php
 include 'templates/pie.php';
